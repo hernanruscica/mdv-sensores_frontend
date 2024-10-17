4277dcf9eee2375aeb2fcbf3db71d1fdd2ece5e2
cesar <cesarhernanruscica@gmail.com>
2024-10-17 11:31:48 -0300

    crollback to old app.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-// src/App.jsx
+// src/App.js
 import React from 'react';
 import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
@@ -21,25 +21,27 @@ import ViewAlarm from './pages/Alarms/ViewAlarm.jsx';
 import PrivateRoute from './components/PrivateRoute';
 
 const App = () => {
+  
   return (
     <AuthProvider>
       <DashboardProvider>
-        <Router basename={import.meta.env.BASE_URL}>
+        <Router>
           <Header />
           <Routes>
             <Route path="/" element={<Home />} />
             <Route path="/inicio" element={<Login />} />
-            <Route path="/panel" element={<PrivateRoute><Dashboard /></PrivateRoute>} />
-            <Route path="/panel/usuarios" element={<PrivateRoute><Users /></PrivateRoute>} />
-            <Route path="/panel/usuarios/:id" element={<PrivateRoute><ViewUser /></PrivateRoute>} />
-            <Route path="/panel/ubicaciones" element={<PrivateRoute><Locations /></PrivateRoute>} />       
-            <Route path="/panel/ubicaciones/:id" element={<PrivateRoute><ViewLocation /></PrivateRoute>} />     
-            <Route path="/panel/dataloggers" element={<PrivateRoute><Dataloggers /></PrivateRoute>} />          
-            <Route path="/panel/dataloggers/:id" element={<PrivateRoute><ViewDatalogger /></PrivateRoute>} />    
-            <Route path="/panel/dataloggers/:id/canales" element={<PrivateRoute><Channels /></PrivateRoute>} />    
-            <Route path="/panel/dataloggers/:id/canales/:channelId" element={<PrivateRoute><ViewChannel /></PrivateRoute>} /> 
-            <Route path="/panel/dataloggers/:id/canales/:channelId/alarmas" element={<PrivateRoute><Alarms /></PrivateRoute>} />      
-            <Route path="/panel/dataloggers/:id/canales/:channelId/alarmas/:alarmId" element={<PrivateRoute><ViewAlarm /></PrivateRoute>} />   
+              <Route path="/panel" element={<PrivateRoute ><Dashboard /></PrivateRoute>} />
+              <Route path="/panel/usuarios" element={<PrivateRoute ><Users /></PrivateRoute>} />
+              <Route path="/panel/usuarios/:id" element={<PrivateRoute ><ViewUser /></PrivateRoute>} />
+              <Route path="/panel/ubicaciones" element={<PrivateRoute ><Locations /></PrivateRoute>} />       
+              <Route path="/panel/ubicaciones/:id" element={<PrivateRoute ><ViewLocation /></PrivateRoute>} />     
+              <Route path="/panel/usuarios/:id" element={<PrivateRoute ><ViewUser /></PrivateRoute>} />    
+              <Route path="/panel/dataloggers" element={<PrivateRoute ><Dataloggers /></PrivateRoute>} />          
+              <Route path="/panel/dataloggers/:id" element={<PrivateRoute ><ViewDatalogger /></PrivateRoute>} />    
+              <Route path="/panel/dataloggers/:id/canales" element={<PrivateRoute ><Channels /></PrivateRoute>} />    
+              <Route path="/panel/dataloggers/:id/canales/:channelId" element={<PrivateRoute ><ViewChannel /></PrivateRoute>} /> 
+              <Route path="/panel/dataloggers/:id/canales/:channelId/alarmas" element={<PrivateRoute ><Alarms /></PrivateRoute>} />      
+              <Route path="/panel/dataloggers/:id/canales/:channelId/alarmas/:alarmId" element={<PrivateRoute ><ViewAlarm /></PrivateRoute>} />   
           </Routes>
         </Router>
       </DashboardProvider>
